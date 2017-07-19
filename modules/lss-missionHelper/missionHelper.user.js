90b3a9d562e12d5b21800e5ea47ebcb8f3c77f77
Jalibu <dev@localhost>
2017-07-19 16:37:36 +0200

    some tiny improvements

--- modules/lss-missionHelper/missionHelper.user.js
+++ modules/lss-missionHelper/missionHelper.user.js
@@ -1,62 +1,65 @@
-(function() {
-	function setValues() {
-		I18n.translations.de['lssm']['missionHelper'] = {
-			carmh: 'Benötigte Fahrzeuge'
-		}
+(function () {
+    function setValues() {
+        I18n.translations.de['lssm']['missionHelper'] = {
+            carmh: 'Benötigte Fahrzeuge',
+            transport_probability: 'Transportwahrscheinlichkeit',
+            credits: 'Credits'
+        }
 
-		I18n.translations.en['lssm']['missionHelper'] = {
-			carmh: 'Required vehicles:'
-		}
+        I18n.translations.en['lssm']['missionHelper'] = {
+            carmh: 'Required vehicles:'
+        }
 
-		I18n.translations.nl['lssm']['missionHelper'] = {
-			carmh: 'Benodigde Voertuigen:'
-		}
-	}
+        I18n.translations.nl['lssm']['missionHelper'] = {
+            carmh: 'Benodigde Voertuigen:'
+        }
+    }
 
-	var missionHelp = $('#mission_help');
+    var missionHelp = $('#mission_help');
 
-	if (missionHelp.length > 0) {
-		setValues();
-		var missionData;
-		var vehicleData;
-		var missionId = missionHelp.attr('href').split("/").pop();
-		$.when(
-			$.getJSON(lssm.getlink("/services/missionCrawler/crawlResults.json"), function(data) {
-				missionData = data;
-			}), $.getJSON(lssm.getlink("/services/missionCrawler/carsById.json"), function(data) {
-				vehicleData = data;
-			})).then(function() {
-			var mission = missionData[missionId];
-			if (mission) {
-				var aaoText = I18n.t('lssm.missionHelper.missions.' + missionId);
-				var markup = '<div class="alert alert-warning">';
-				markup += '<h3>' + I18n.t('lssm.missionHelper.carmh') + '</h3>';
-				markup += '<div>';
-				for (var vehicleId in mission.vehicles) {
-					var vehicle = mission.vehicles[vehicleId];
-					vehicle.probability = (vehicle.probability) ? vehicle.probability : 100;
-					vehicle.required = (vehicle.required) ? vehicle.required : 'mind. 1';
-					markup += vehicle.required + "x " + vehicleData[vehicleId][0] + ' (' + vehicle.probability + '%)<br>';
-				};
-				markup += '</div>';
-				if (mission.avg_creds) {
-					markup += '<span class="badge">Credits: ~ ' + mission.avg_creds + '</span>';
-				}
-				if (mission.patients && mission.patients.probability) {
-					markup += '<span class="badge">Transportwahrscheinlichkeit: ' + mission.patients.probability + ' %</span>';
-				}
-				markup += '</div>';
-				$('#mission-form').prepend(markup);
-			}
-		});
-	}
+    if (missionHelp.length > 0) {
+        setValues();
+        var markup = '<div class="alert alert-warning">';
+        markup += '<h3>' + I18n.t('lssm.missionHelper.carmh') + '</h3>';
+        markup += '<span id="lssm-missionhelper-spinner"><span class="glyphicon glyphicon-refresh spinning"></span> Loading...</span>';
+        markup += '<div id="lssm-missionhelper-content">';
+        markup += '</div>';
+        markup += '</div>';
+        $('#mission-form').prepend(markup);
 
-	function request1() {
-		return $.getJSON(lssm.getlink("/services/missionCrawler/crawlResults.json"));
-	}
+        var missionData;
+        var vehicleData;
 
-	function request2() {
-		return $.getJSON(lssm.getlink("/services/missionCrawler/carsById.json"));
-	}
+        $.when(
+            $.getJSON(lssm.getlink("/services/missionCrawler/crawlResults.json"), function (data) {
+                missionData = data;
+            }), $.getJSON(lssm.getlink("/services/missionCrawler/carsById.json"), function (data) {
+                vehicleData = data;
+            })).then(function () {
+            var missionId = missionHelp.attr('href').split("/").pop();
+
+            var mission = missionData[missionId - 1]; // Reduce by 1 due array is zero based
+
+            if (mission) {
+                var markup = '<div>';
+                for (var vehicleId in mission.vehicles) {
+                    var vehicle = mission.vehicles[vehicleId];
+                    vehicle.probability = (vehicle.probability) ? vehicle.probability : 100;
+                    vehicle.required = (vehicle.required) ? vehicle.required : '?';
+                    markup += vehicle.required + "x " + vehicleData[vehicleId][0] + ' (' + vehicle.probability + '%)<br>';
+                };
+                markup += '</div>';
+                if (mission.avg_creds) {
+                    markup += '<span class="badge">' + I18n.t('lssm.missionHelper.credits') + ': ~ ' + mission.avg_creds + '</span>';
+                }
+                if (mission.patients && mission.patients.probability) {
+                    markup += '<span class="badge">' + I18n.t('lssm.missionHelper.transport_probability') + ': ' + mission.patients.probability +
+                        ' % (' + mission.patients.department + ')</span>';
+                }
+                $('#lssm-missionhelper-content').append(markup);
+                $('#lssm-missionhelper-spinner').hide();
+            }
+        });
+    }
 
 })();
